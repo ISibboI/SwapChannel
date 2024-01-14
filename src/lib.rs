4184f67d3017ab81a6b753047d9ee3af2e89b35e
Sebastian Schmidt <isibboi@gmail.com>
2024-01-14 08:19:47 +0200

    Test, document and release 0.1.0.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,12 @@ use std::sync::atomic::Ordering;
 
 static MASTER_KEY_EXISTS: AtomicBool = AtomicBool::new(false);
 
+/// The master key.
+/// Only one instance of this type can exist at any time.
+///
+/// A master key can be used to derive a [DataKey] or a [ChannelKey],
+/// but only at most one derived key can exist simultaneously,
+/// and specifically not both a data key and a channel key at the same time.
 pub struct MasterKey {
     /// A meaningless private field to ensure that
     /// this struct can only be constructed via its constructor.
@@ -13,6 +19,8 @@ pub struct MasterKey {
 }
 
 impl MasterKey {
+    /// Creates a new master key.
+    /// If there already is an existing master key, this function **panics**.
     pub fn create() -> Self {
         // Assert that the master key does not exist
         // and set it as existing.
@@ -22,12 +30,16 @@ impl MasterKey {
         Self { dummy: () }
     }
 
+    /// Get a unique data key from this master key.
+    /// The data key mutably borrows from the master key, hence there can be no other keys at the same time.
     pub fn get_data_key(&mut self) -> DataKey<'_> {
         DataKey {
             scope: Default::default(),
         }
     }
 
+    /// Get a unique channel key from this master key.
+    /// The channel key mutably borrows from the master key, hence there can be no other keys at the same time.
     pub fn get_channel_key(&mut self) -> ChannelKey<'_> {
         ChannelKey {
             scope: Default::default(),
@@ -36,6 +48,7 @@ impl MasterKey {
 }
 
 impl Drop for MasterKey {
+    /// After dropping a master key, a new one can be created again.
     fn drop(&mut self) {
         // Assert that the master key exists
         // and set it as not existing.
@@ -43,30 +56,68 @@ impl Drop for MasterKey {
     }
 }
 
+/// The key used for accessing a [DataPointer].
+/// Only one can simultaneously exist at any point, and only if there is no channel key.
 pub struct DataKey<'master_key> {
     scope: PhantomData<&'master_key mut MasterKey>,
 }
 
+/// The key used for accessing a [ChannelPointer].
+/// Only one can simultaneously exist at any point, and only if there is no data key.
 pub struct ChannelKey<'master_key> {
     scope: PhantomData<&'master_key mut MasterKey>,
 }
 
+impl<'master_key> DataKey<'master_key> {
+    /// Convert this data key into a channel key.
+    /// This consumes the data key, ensuring that there is never both a channel key and a data key.
+    pub fn into_channel_key(self) -> ChannelKey<'master_key> {
+        ChannelKey { scope: self.scope }
+    }
+}
+
+impl<'master_key> ChannelKey<'master_key> {
+    /// Convert this channel key into a data key.
+    /// This consumes the channel key, ensuring that there is never both a channel key and a data key.
+    pub fn into_data_key(self) -> DataKey<'master_key> {
+        DataKey { scope: self.scope }
+    }
+}
+
+/// A channel used for communication between threads.
+/// It holds to instances of `Data`, which can be accessed or swapped.
+/// At any time, either references to `Data` can exist, or a swap operation can be performed.
+/// This allows to different threads to hold pointers to one of the `Data` fields each,
+/// and a third thread to swap the content of these `Data` fields, resulting in inter-thread communication.
+///
+/// See [Channel::create] for more info.
 pub struct Channel<Data> {
     data1: Data,
     data2: Data,
 }
 
+/// A pointer to a channel.
+/// It can only be accessed using a [ChannelKey].
+///
+/// This type should always be destroyed via the [Channel::destroy] or [ChannelPointer::destroy] method to ensure soundness (at runtime).
 #[must_use]
 pub struct ChannelPointer<Data> {
     channel: Box<Channel<Data>>,
 }
 
+/// A pointer to one of the data fields in a channel.
+/// It can only be accessed using a [DataKey].
+///
+/// This type should always be destroyed via the [Channel::destroy] or [ChannelPointer::destroy] method to ensure soundness (at runtime).
 #[must_use]
 pub struct DataPointer<Data> {
     data: *mut Data,
 }
 
 impl<Data> Channel<Data> {
+    /// Create a channel and hand out three pointers to it.
+    /// One [ChannelPointer] used to swap the content of the two `Data` fields,
+    /// and two [DataPointer]s, one to each data field.
     pub fn create(
         data1: Data,
         data2: Data,
@@ -83,11 +134,14 @@ impl<Data> Channel<Data> {
         (channel_pointer, data_pointer1, data_pointer2)
     }
 
+    /// Destroys the channel linked with the three pointers (see [Channel::create]).
+    ///
+    /// **Panics** if not all three pointers point to the same channel.
     pub fn destroy(
         channel_pointer: ChannelPointer<Data>,
         data_pointer1: DataPointer<Data>,
         data_pointer2: DataPointer<Data>,
-    ) {
+    ) -> (Data, Data) {
         let ChannelPointer { mut channel } = channel_pointer;
         let channel_data_pointer1 = (&mut channel.data1) as *mut Data;
         let channel_data_pointer2 = (&mut channel.data2) as *mut Data;
@@ -102,23 +156,37 @@ impl<Data> Channel<Data> {
             (channel_data_pointer1 == data_pointer1 && channel_data_pointer2 == data_pointer2)
                 || (channel_data_pointer1 == data_pointer2
                     && channel_data_pointer2 == data_pointer1)
-        )
+        );
+
+        (channel.data1, channel.data2)
     }
 }
 
 impl<Data> ChannelPointer<Data> {
-    pub fn swap(&mut self, _key: ChannelKey) {
+    /// Swap the two `Data` fields in the channel.
+    pub fn swap(&mut self, _key: &ChannelKey) {
         let channel: &mut Channel<Data> = &mut self.channel;
         mem::swap(&mut channel.data1, &mut channel.data2);
+    }
+
+    /// Shorthand for [Channel::destroy].
+    pub fn destroy(
+        self,
+        data_pointer1: DataPointer<Data>,
+        data_pointer2: DataPointer<Data>,
+    ) -> (Data, Data) {
+        Channel::destroy(self, data_pointer1, data_pointer2)
     }
 }
 
 impl<Data> DataPointer<Data> {
-    pub fn get(&self, _key: DataKey) -> &Data {
+    /// Get a reference to the `Data` field pointed to by this pointer.
+    pub fn get(&self, _key: &DataKey) -> &Data {
         unsafe { &*self.data }
     }
 
-    pub fn get_mut(&mut self, _key: DataKey) -> &mut Data {
+    /// Get a mutable reference to the `Data` field pointed to by this pointer.
+    pub fn get_mut(&mut self, _key: &DataKey) -> &mut Data {
         unsafe { &mut *self.data }
     }
 }
@@ -129,7 +197,20 @@ mod tests {
 
     #[test]
     fn test() {
-        let master_key = MasterKey::create();
-        let (channel_pointer, data_pointer1, data_pointer2) = Channel::create((), ());
+        let mut master_key = MasterKey::create();
+        let (mut channel_pointer, mut data_pointer1, data_pointer2) = Channel::create(0, 0);
+
+        for _ in 0..3 {
+            let data_key = master_key.get_data_key();
+            let value = *data_pointer1.get(&data_key) * 3 + *data_pointer2.get(&data_key) + 1;
+            *data_pointer1.get_mut(&data_key) = value;
+
+            let channel_key = data_key.into_channel_key();
+            channel_pointer.swap(&channel_key);
+        }
+
+        let (data1, data2) = Channel::destroy(channel_pointer, data_pointer1, data_pointer2);
+        assert_eq!(data1, 2);
+        assert_eq!(data2, 6);
     }
 }
